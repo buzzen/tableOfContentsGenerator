e5600aba9ee4f64dd3c9502cdbe8e8e7e83072d0
buzzen <xawyzen@gmail.com>
2020-06-17 14:28:28 +0800

    first working version.

--- main.js
+++ main.js
@@ -1,29 +1,56 @@
-var headings = document.querySelectorAll("h2, h3, h4, h5, h6");
-
-var tableOfContents = document.createElement("ul");
-
 var anchor_no = 0; // to create id for headings that don't have a id attribute
-for (var i = 0; i < headings.length; i++) {
-    let currentTagName = headings[i].tagName;
-    let previousTagName = null;
-    if (i !== 0) previousTagName = headings[i-1].tagName;
 
-    if (currentTagName === "H2") {
-        createLink(tableOfContents, headings[i]);        
-    } else if (currentTagName === "H3") {
-        if (previousTagName === "H2") {
-            var h3_list = document.createElement("ul");
-            tableOfContents.appendChild(h3_list);
+function addTableOfContents() {
+    if (document.querySelector("ul.tableOfContents")) {
+        return;
+    }
+    var tableOfContents = document.createElement("ul");
+    tableOfContents.classList.add("tableOfContents");
+
+    var headings = document.querySelectorAll("h2, h3, h4, h5, h6");
+
+    for (var i = 0; i < headings.length; i++) {
+        let currentTagName = headings[i].tagName;
+        let previousTagName = null;
+        if (i !== 0) previousTagName = headings[i-1].tagName;
+
+        if (currentTagName === "H2") {
+            createLink(tableOfContents, headings[i]);        
+        } else if (currentTagName === "H3") {
+            if (previousTagName === "H2") {
+                var h3_list = document.createElement("ul");
+                tableOfContents.appendChild(h3_list);
+            }
+
+            createLink(h3_list, headings[i]);
+        } else if (currentTagName === "H4") {
+            if (previousTagName === "H3") {
+                var h4_list = document.createElement("ul");
+                h3_list.appendChild(h4_list);
+            }
+
+            createLink(h4_list, headings[i]);
+        } else if (currentTagName === "H5") {
+            if (previousTagName === "H4") {
+                var h5_list = document.createElement("ul");
+                h4_list.appendChild(h5_list);
+            }
+
+            createLink(h5_list, headings[i]);
+        } else if (currentTagName === "H6") {
+            if (previousTagName === "H5") {
+                var h6_list = document.createElement("ul");
+                h5_list.appendChild(h6_list);
+            }
+
+            createLink(h6_list, headings[i]);
         }
 
-        createLink(h3_list, headings[i]);
     }
 
-    /* case of h4 h5 h6 */
+    var body = document.querySelector("body");
+    body.prepend(tableOfContents);
 }
-
-var body = document.querySelector("body");
-body.prepend(tableOfContents);
 
 function createLink(parent, current) {
     let item = document.createElement("li");
@@ -44,3 +71,5 @@ function createLink(parent, current) {
     item.appendChild(link);
     parent.appendChild(item);
 }
+
+addTableOfContents();
